fa8d91ac2d865329e7320bc92b3b65e9ecc76c05
Nidhin krishna G <nidhingoplank@gmail.com>
2023-03-10 09:09:52 +0530

    login and logout functionality added

--- notesapp/base/urls.py
+++ notesapp/base/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.Homepage,name='home'),
     path('signup/',views.signup,name='signup'),
+    path('login/',views.loginPage,name='login'),
+    path('logout/',views.logoutfunction,name='logout'),
     
 ]
--- notesapp/base/views.py
+++ notesapp/base/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,redirect
 from .forms import *
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 # Create your views here.
+@login_required(login_url='login')
 def Homepage(request):
 
 
@@ -20,6 +24,40 @@ def signup(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request,'registration success you can login now')
             return redirect('/')
+         
 
     return render(request,'signup.html',context)
+  
+
+
+
+
+def loginPage(request):
+
+    if request.method =="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request,'login success')
+            return redirect('/')
+        else:
+            messages.error(request,'invalid username')
+        
+
+    return render(request,'login.html')
+
+
+def logoutfunction(request):
+
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,'succesfully loged out ')
+        return redirect('login')
+
+    
